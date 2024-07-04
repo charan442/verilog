04f8db48bb88474b552aa505c500f0395abb0b80
charan442 <85566214+charan442@users.noreply.github.com>
2024-07-04 15:00:28 +0530

    Update Assignment -2.v

--- Verilog Assignment /Assignment -2.v
+++ Verilog Assignment /Assignment -2.v
@@ -6,8 +6,8 @@ begin
 y= &#39;Ox2;
 end
 Ans:
-The hexadecimal value 'Ox2 is equivalent to the binary value 000010.
-Therefore, the value of y in binary format is 0010.
+The hexadecimal value 'Ox2 is equivalent to the binary value 0010.
+Therefore, the value of y in binary format is 6'b000010.
 
 2. What is the value of the b in the below snippet?
 
@@ -25,4 +25,73 @@ module test(output [32:0]b);
 real a=14.5;
 assign b = a;
 endmodule
+Ans: port b 14
+
+4. Find the value of the variable t at 1ns &amp; 20ns respectively from the
+below snippet.
+time = t;
+initial begin
+#10 t= $time;
+#20 ;
+end
+Ans: t =$time 10ns.
+
+5. Write an RTL snippet to initialize all the locations of an array of size:10x8 to
+0 at 0ns. Depth= 10, Width = 8bits
 Ans:
+module array_initialization (
+  input wire clk,
+  input wire rst,
+  output reg [7:0] array [9:0]
+);
+
+integer i;
+
+always @(posedge clk or posedge rst) begin
+  if (rst) begin
+    for (i = 0; i < 10; i = i + 1) begin
+      array[i] <= 8'b0;
+    end
+  end
+end
+
+endmodule
+
+6. What will be the value of the parameter constant for the instance DUT in
+the top module from the below snippet?
+module test;
+parameter WIDTH=8;
+endmodule
+module top;
+test #(16) OUT ( )
+;
+endmodule
+Ans:The value of the parameter WIDTH for the instance DUT in the top module is 16.
+
+7. What is the bug in the following snippet?
+module test;
+parameter WIDTH=8;
+
+endmodule
+module top;
+test OUT ( ) ;
+initial
+defparam OUT.WIDTH=10;
+endmodule
+Ans:
+  parameters should be set explicitly at module instantiation rather than using defparam statements inside procedural blocks like 'initial'.
+
+8. What will be displayed in the console mode for the below snippet?
+
+reg[13*8:1]s;
+initial
+begin
+s = &quot;hello world&quot;;
+$display(&quot;Value = %s&quot;,s[104:65]);
+
+end
+    Ans:
+    value = hello
+
+  
+  
